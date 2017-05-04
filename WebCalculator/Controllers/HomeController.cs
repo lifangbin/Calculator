078facc64fa90e981b2b9dcd299ccc307594972d
lifangbin <fangbin.li@outlook.com>
2017-05-04 13:54:21 +0800

    Add files via upload

---
+++ WebCalculator/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+using Calculator;
+using System;
+
+namespace WebCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string expression)
+        {
+            if (!string.IsNullOrEmpty(expression))
+            {
+                Calculator.Calculator cal = new Calculator.Calculator(expression)
+                {
+                    Mode = CalculatorMode.Mathematics
+                };
+                ViewBag.Expression = expression;
+                try
+                {
+                    ViewBag.Expression = cal.Calculate().ToString();
+
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Expression = ex.Message;
+                }
+            }
+            return this.View();
+        }
+
+    }
+}
---
+++ WebCalculator/Scripts/WebCalculator.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    // Get all the keys from document
+    var keys = document.querySelectorAll('#calculator span');
+    
+    // Add onclick event to all the keys and perform operations
+    for (var i = 0; i < keys.length; i++) {
+        keys[i].onclick = function (e) {
+            // Get the input and button values
+            var input = document.querySelector('.screen');
+            var inputVal = input.value;
+            var btnVal = this.innerHTML;
+            
+
+            // Now, just append the key values (btnValue) to the input string and finally use javascript's eval function to get the result
+            // If clear key is pressed, erase everything
+            if (btnVal == 'C') {
+                input.value = '';
+            }
+            else {
+                input.value += btnVal;
+            }
+            e.preventDefault();
+        }
+    }
+});
