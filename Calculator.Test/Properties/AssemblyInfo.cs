494dd65f41964588352729d3bcc2e2bf372b4c50
lifangbin <fangbin.li@outlook.com>
2017-05-04 13:46:50 +0800

    Add files via upload

---
+++ Calculator.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Calculator.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyProduct("Calculator.Test")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7ee8634d-ec9f-44bc-8d33-b5e8c77b3a85")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Calculator.Test/UnitTest1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ExpressionEvaluate()
+        {
+            var expressions = new[]
+            {
+                "2+3-4",
+                "-1+5-4",
+                "2*3+4",
+                "2*(3+5)",
+                "2*(3+5)/6",
+                "2*(2*(2*(2+1)))",
+                "10%3",
+                "1+16.5/(2+gauss(5,2,3,1))",
+                "sqrt(6)",
+                "abs(-7)",
+                "floor(1.5)",
+            };
+
+            foreach (string expression in expressions)
+            {
+                Calculator cal = new Calculator(expression)
+                {
+                    Mode = CalculatorMode.Mathematics
+                };
+
+                Console.WriteLine("{0} = {1}", expression, cal.Calculate());
+            }
+
+        }
+
+        [TestMethod]
+        public void ShouldHaveErrorMessages()
+        {
+            var expressions = new[]
+            {
+                "(2+3+4",
+                "1+5-4-",
+                "2*(3+5)/0",
+                "%3",
+                "abc(123)",
+                "1+16.5/(2+gauss(5,1,0,1))" // NaN means zero by zero
+            };
+
+            foreach (string expression in expressions)
+            {
+                Calculator cal = new Calculator(expression)
+                {
+                    Mode = CalculatorMode.Mathematics
+                };
+
+                try
+                {
+                    Console.WriteLine("{0} = {1}", expression, cal.Calculate());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("{0} = {1}", expression, ex.Message);
+                }
+            }
+        }
+    }
+}
